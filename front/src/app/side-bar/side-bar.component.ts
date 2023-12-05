7c319cf7da572a9163cebf15ae79183b42269292
Aaabdou <101267176+Aaabdou@users.noreply.github.com>
2023-12-05 08:21:08 +0100

    mis à our

--- front/src/app/side-bar/side-bar.component.ts
+++ front/src/app/side-bar/side-bar.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-side-bar',
+  standalone: true,
   templateUrl: './side-bar.component.html',
   styleUrls: ['./side-bar.component.css']
 })
