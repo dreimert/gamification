260d273f1d7ad0df1db7ec534ff95daff6a138e7
ghoti-GUI <guichijin@gmail.com>
2023-12-06 22:42:15 +0100

    update logos and routes

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,11 +1,17 @@
 import { Routes } from '@angular/router';
 import {AccueilComponent} from './mainpage/accueil/accueil.component';
 import {NotesComponent} from './mainpage/notes/notes.component';
+import { SessionComponent } from './mainpage/session/session.component';
 export const routes: Routes = [
       {
         path: 'notes',
         component: NotesComponent,
         title: 'Note',
+      },
+      {
+        path: 'session',
+        component: SessionComponent,
+        title: 'Session',
       },
       {
         path: '',
