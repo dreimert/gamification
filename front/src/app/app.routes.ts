448d002f3aedda8621d00b9bf3249cfa91652782
Aaabdou <101267176+Aaabdou@users.noreply.github.com>
2023-12-06 18:57:48 +0100

    ajout de session dans les routes

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,11 +1,17 @@
 import { Routes } from '@angular/router';
 import {AccueilComponent} from './mainpage/accueil/accueil.component';
 import {NotesComponent} from './mainpage/notes/notes.component';
+import {SessionComponent} from './mainpage/session/session.component';
 export const routes: Routes = [
+      {
+        path: 'sessions',
+        component: SessionComponent,
+        title: 'Sessions',
+      },
       {
         path: 'notes',
         component: NotesComponent,
-        title: 'Note',
+        title: 'Notes',
       },
       {
         path: '',
