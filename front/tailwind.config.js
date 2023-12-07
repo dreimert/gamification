651df2e471186ec723e9de93cfbe744225d232e0
Aaabdou <101267176+Aaabdou@users.noreply.github.com>
2023-12-07 10:04:45 +0100

    add custom colors

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'grey':{
+          light:'#EAEAEA',
+          DEFAULT:'#D7D7D7',
+        },
+        'sidebar':'#45504F',
+        'redButton':'#CF2700',
+        'blueButton':'#004FFF',
+      }
+    },
   },
   plugins: [],
 }
