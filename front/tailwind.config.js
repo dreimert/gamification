d1e273c2ae1d5a822f5e1c825dc58bdb7d19c667
Aaabdou <101267176+Aaabdou@users.noreply.github.com>
2023-12-07 10:27:07 +0100

    added more colors

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -7,12 +7,16 @@ module.exports = {
     extend: {
       colors:{
         'grey':{
-          light:'#EAEAEA',
-          DEFAULT:'#D7D7D7',
+          DEFAULT:'#EAEAEA',
+          dark:'#D7D7D7',
         },
         'sidebar':'#45504F',
         'redButton':'#CF2700',
         'blueButton':'#004FFF',
+        'bgHeaderTable':'#C3C3C3',
+        'bgTable':'#D7D7D7',
+        'borderTable':'#9c9c9c',
+        
       }
     },
   },
