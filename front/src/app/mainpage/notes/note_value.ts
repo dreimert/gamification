98ed849b90dde80fcd3d8f84302be66ad5ddef78
Aaabdou <101267176+Aaabdou@users.noreply.github.com>
2023-12-07 10:27:48 +0100

    update notepage

---
+++ front/src/app/mainpage/notes/note_value.ts
@@ -0,0 +1,17 @@
+import { Note } from './note';
+export const notes_stu: Note[] = [
+    {
+        id: 0,
+        name: "TP scraping",
+        note: 14,
+        moyenne:15,
+        coefficient:2,
+    },
+    {
+        id: 1,
+        name: "TP Kafka",
+        note: 12,
+        moyenne:10,
+        coefficient:1,
+    }
+]
