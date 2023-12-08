9d3bfd2fc84f0cd19695ed6bec7886e446f16f7c
ghoti-GUI <guichijin@gmail.com>
2023-12-08 11:28:28 +0100

    update: adjust img and text size

--- front/src/app/mainpage/session/join/join.component.ts
+++ front/src/app/mainpage/session/join/join.component.ts
@@ -5,8 +5,6 @@ import { CommonModule } from "@angular/common";
 import { calculateFontSize } from "../utils";
 import { Session, TeacherSession } from "../../../models/session.model";
 
-import { Session, sessions } from '../session';
-
 
 @Component({
     selector: "app-join",
