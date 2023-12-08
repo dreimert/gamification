868210c93bd90df1b18c8f47b42e96ff7bb664c1
Aaabdou <101267176+Aaabdou@users.noreply.github.com>
2023-12-08 15:28:03 +0100

    init adminPage

--- front/src/app/mainpage/admin/admin.component.ts
+++ front/src/app/mainpage/admin/admin.component.ts
@@ -1,10 +1,15 @@
-import { Component } from "@angular/core";
-
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { Header } from '../header/header';
 @Component({
-    selector: "app-admin",
-    standalone: true,
-    imports: [],
-    templateUrl: "./admin.component.html",
-    styleUrl: "./admin.component.css",
+  selector: 'app-admin',
+  standalone: true,
+  imports: [HeaderComponent],
+  templateUrl: './admin.component.html',
+  styleUrl: './admin.component.css'
 })
-export class AdminComponent {}
+export class AdminComponent {
+  section : Header={
+    name:"Admin",
+  }
+}
