d79c9fa3dd55ea63b81dc1e0aaeb0d091d119205
ghoti-GUI <guichijin@gmail.com>
2023-12-19 19:39:28 +0100

    update test file

--- front/src/app/mainpage/session/avancement/avancement.component.spec.ts
+++ front/src/app/mainpage/session/avancement/avancement.component.spec.ts
@@ -1,14 +1,42 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AvancementComponent } from './avancement.component';
+import { RouterTestingModule } from "@angular/router/testing";
+import { Observable, of } from "rxjs";
+import { Session, SessionStatus, TeacherSession } from '../../../models/session.model';
+import { SessionService } from '../../../services/session.service';
+
+
+
+let sessionServiceStub: Partial<SessionService>;
 
 describe('AvancementComponent', () => {
+  sessionServiceStub = {
+    getAvailableSessions(): Observable<Session[] | TeacherSession[]> {
+        return of<Session[]>([
+            {
+                id: "1",
+                name: "test",
+                teachers: ["1"],
+                startDate: new Date(),
+                endDate: new Date(Date.now() + 1000 * 60 * 60 * 2),
+                TP: "1",
+                status: SessionStatus.SCHEDULED,
+            },
+        ]);
+    },
+  };
+
+
   let component: AvancementComponent;
   let fixture: ComponentFixture<AvancementComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AvancementComponent]
+      imports: [AvancementComponent, RouterTestingModule],
+      providers:[
+        { provide: SessionService, useValue: sessionServiceStub },
+      ]
     })
     .compileComponents();
     
--- front/src/app/mainpage/session/avancement/modification-avancement/modification-avancement.component.spec.ts
+++ front/src/app/mainpage/session/avancement/modification-avancement/modification-avancement.component.spec.ts
@@ -1,14 +1,41 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ModificationAvancementComponent } from './modification-avancement.component';
+import { RouterTestingModule } from "@angular/router/testing";
+import { Observable, of } from "rxjs";
+import { Session, SessionStatus, TeacherSession } from '../../../../models/session.model';
+import { SessionService } from '../../../../services/session.service';
+
+
+let sessionServiceStub: Partial<SessionService>;
 
 describe('ModificationAvancementComponent', () => {
+  sessionServiceStub = {
+    getAvailableSessions(): Observable<Session[] | TeacherSession[]> {
+        return of<Session[]>([
+            {
+                id: "1",
+                name: "test",
+                teachers: ["1"],
+                startDate: new Date(),
+                endDate: new Date(Date.now() + 1000 * 60 * 60 * 2),
+                TP: "1",
+                status: SessionStatus.SCHEDULED,
+            },
+        ]);
+    },
+  };
+
+
   let component: ModificationAvancementComponent;
   let fixture: ComponentFixture<ModificationAvancementComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ModificationAvancementComponent]
+      imports: [ModificationAvancementComponent, RouterTestingModule],
+      providers:[
+        { provide: SessionService, useValue: sessionServiceStub },
+      ]
     })
     .compileComponents();
     
