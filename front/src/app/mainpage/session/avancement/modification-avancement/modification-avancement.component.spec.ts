44f628b54c4ada4856e2fb51412ad0578f939ead
ghoti-GUI <guichijin@gmail.com>
2023-12-20 10:37:58 +0100

    lint

--- front/src/app/mainpage/session/avancement/modification-avancement/modification-avancement.component.spec.ts
+++ front/src/app/mainpage/session/avancement/modification-avancement/modification-avancement.component.spec.ts
@@ -1,50 +1,45 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { ModificationAvancementComponent } from './modification-avancement.component';
+import { ModificationAvancementComponent } from "./modification-avancement.component";
 import { RouterTestingModule } from "@angular/router/testing";
 import { Observable, of } from "rxjs";
-import { Session, SessionStatus, TeacherSession } from '../../../../models/session.model';
-import { SessionService } from '../../../../services/session.service';
-
+import { Session, SessionStatus, TeacherSession } from "../../../../models/session.model";
+import { SessionService } from "../../../../services/session.service";
 
 let sessionServiceStub: Partial<SessionService>;
 
-describe('ModificationAvancementComponent', () => {
-  sessionServiceStub = {
-    getAvailableSessions(): Observable<Session[] | TeacherSession[]> {
-        return of<Session[]>([
-            {
-                id: "1",
-                name: "test",
-                teachers: ["1"],
-                startDate: new Date(),
-                endDate: new Date(Date.now() + 1000 * 60 * 60 * 2),
-                TP: "1",
-                status: SessionStatus.SCHEDULED,
-            },
-        ]);
-    },
-  };
+describe("ModificationAvancementComponent", () => {
+    sessionServiceStub = {
+        getAvailableSessions(): Observable<Session[] | TeacherSession[]> {
+            return of<Session[]>([
+                {
+                    id: "1",
+                    name: "test",
+                    teachers: ["1"],
+                    startDate: new Date(),
+                    endDate: new Date(Date.now() + 1000 * 60 * 60 * 2),
+                    TP: "1",
+                    status: SessionStatus.SCHEDULED,
+                },
+            ]);
+        },
+    };
 
+    let component: ModificationAvancementComponent;
+    let fixture: ComponentFixture<ModificationAvancementComponent>;
 
-  let component: ModificationAvancementComponent;
-  let fixture: ComponentFixture<ModificationAvancementComponent>;
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [ModificationAvancementComponent, RouterTestingModule],
+            providers: [{ provide: SessionService, useValue: sessionServiceStub }],
+        }).compileComponents();
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [ModificationAvancementComponent, RouterTestingModule],
-      providers:[
-        { provide: SessionService, useValue: sessionServiceStub },
-      ]
-    })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(ModificationAvancementComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(ModificationAvancementComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it("should create", () => {
+        expect(component).toBeTruthy();
+    });
 });
--- front/src/app/mainpage/session/avancement/modification-avancement/modification-avancement.component.ts
+++ front/src/app/mainpage/session/avancement/modification-avancement/modification-avancement.component.ts
@@ -1,115 +1,119 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule  } from '@angular/common';
-import { RouterLink } from '@angular/router';
-import { AbstractControl, FormBuilder, ReactiveFormsModule, ValidationErrors, Validators, FormControl } from "@angular/forms";
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { Observable } from 'rxjs';
-import { startWith, map } from 'rxjs/operators';
-import { HeaderComponent } from '../../../header/header.component';
-import { Header } from '../../../header/header';
-import { SessionService } from '../../../../services/session.service';
-import { TeacherSession, Session } from '../../../../models/session.model';
-import { niveaux, avancements } from '../avancement';
+import { Component, OnInit } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterLink } from "@angular/router";
+import { AbstractControl, FormBuilder, ReactiveFormsModule, ValidationErrors, Validators } from "@angular/forms";
+import { MatAutocompleteModule } from "@angular/material/autocomplete";
+import { MatInputModule } from "@angular/material/input";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { Observable } from "rxjs";
+import { startWith, map } from "rxjs/operators";
+import { HeaderComponent } from "../../../header/header.component";
+import { Header } from "../../../header/header";
+import { SessionService } from "../../../../services/session.service";
+import { TeacherSession, Session } from "../../../../models/session.model";
+import { niveaux, avancements } from "../avancement";
 
 @Component({
-  selector: 'app-modification-avancement',
-  standalone: true,
-  imports: [CommonModule, HeaderComponent, ReactiveFormsModule, RouterLink,  MatAutocompleteModule, MatInputModule, MatFormFieldModule],
-  templateUrl: './modification-avancement.component.html',
-  styleUrl: './modification-avancement.component.css'
+    selector: "app-modification-avancement",
+    standalone: true,
+    imports: [
+        CommonModule,
+        HeaderComponent,
+        ReactiveFormsModule,
+        RouterLink,
+        MatAutocompleteModule,
+        MatInputModule,
+        MatFormFieldModule,
+    ],
+    templateUrl: "./modification-avancement.component.html",
+    styleUrl: "./modification-avancement.component.css",
 })
 export class ModificationAvancementComponent implements OnInit {
+    session!: Session | TeacherSession;
+    section: Header = { name: `Session/SESSION_NAME/avancement/modifier` };
 
-  session!: Session | TeacherSession;
-  section: Header = {name:`Session/SESSION_NAME/avancement/modifier`};
+    filteredOptions!: Observable<string[]> | undefined;
 
-  filteredOptions!: Observable<string[]> | undefined;
+    constructor(
+        private sessionService: SessionService,
+        private formBuilder: FormBuilder,
+    ) {}
 
-  constructor(
-    private sessionService: SessionService,
-    private formBuilder: FormBuilder,
-  ){}
+    segments = window.location.href.split("/");
+    sessionId =
+        decodeURIComponent(this.segments[this.segments.length - 3].replace(/%20/g, " ")) ||
+        "URL does not have enough segments.";
+    ngOnInit(): void {
+        // get session info
+        this.sessionService.getAvailableSessions().subscribe((sessions: Session[] | TeacherSession[]) => {
+            const sessionInfo = sessions.find((s: Session | TeacherSession) => s.id == this.sessionId);
+            if (sessionInfo) {
+                this.session = sessionInfo;
+            } else {
+                console.log("name of session not found");
+            }
+            this.section = {
+                name: `Session/${this.session.name}/avancement/modifier`,
+            };
+        });
 
-  segments = window.location.href.split('/');
-  sessionId = decodeURIComponent(this.segments[this.segments.length - 3].replace(/%20/g, ' ')) || 'URL does not have enough segments.';
-  ngOnInit(): void {
-    // get session info
-    this.sessionService.getAvailableSessions().subscribe((sessions: Session[] | TeacherSession[] ) => {
-      const sessionInfo = sessions.find((s: Session | TeacherSession) => s.id == this.sessionId);
-      if (sessionInfo){
-        this.session = sessionInfo;
-      }
-      else{
-        console.log("name of session not found");
-      }
-      this.section = {
-        name:`Session/${this.session.name}/avancement/modifier`
-      }
+        // filter studient
+        this.filteredOptions = this.avancementForm.get("name")?.valueChanges.pipe(
+            startWith(""),
+            map((value) => this._filter(value)),
+        );
+    }
+
+    avancementForm = this.formBuilder.group({
+        name: ["", [Validators.required, this.validateName.bind(this)]],
+        avancement: ["", [Validators.required, this.validateAvance.bind(this)]],
     });
 
-    // filter studient
-    this.filteredOptions = this.avancementForm.get('name')?.valueChanges.pipe(
-      startWith(''),
-      map(value => this._filter(value))
-    );
-  }
+    // change form of studients data
+    niveaux = Object.entries(niveaux).map(([key, value], index) => {
+        return { id: index, key: key, value: value };
+    });
+    avancements = [...avancements];
+    studients: { [key: string]: string } = this.avancements.reduce((result: Record<string, string>, avancement) => {
+        result[avancement.name] = avancement.niveau;
+        return result;
+    }, {});
+    studientsList = Object.keys(this.studients);
 
-  avancementForm = this.formBuilder.group(
-    {
-      name: ['', [Validators.required, this.validateName.bind(this)]],
-      avancement: ['', [Validators.required, this.validateAvance.bind(this)]]
-    },
-  )
+    StuAvance!: string | null;
 
-  // change form of studients data
-  niveaux = Object.entries(niveaux).map(([key, value], index)=>{
-    return{ id:index, key:key, value:value, }
-  })
-  avancements = [...avancements]
-  studients: { [key:string]:string } = this.avancements.reduce((result: Record<string, string>, avancement)=>{
-    result[avancement.name] = avancement.niveau;
-    return result;
-  }, {})
-  studientsList = Object.keys(this.studients);
-
-
-  StuAvance!: string | null ;
-
-  validateName(control: AbstractControl):ValidationErrors | null{
-    const name:string = control.value;
-    if(name){
-      this.StuAvance = this.studients[name];
+    validateName(control: AbstractControl): ValidationErrors | null {
+        const name: string = control.value;
+        if (name) {
+            this.StuAvance = this.studients[name];
+        }
+        if (!name || this.studientsList.includes(name)) {
+            return null;
+        } else {
+            control.setErrors({ nameError: "Le nom entré n'existe pas" });
+            return { nameError: "Le nom entré n'existe pas" };
+        }
     }
-    if (!name || this.studientsList.includes(name)){
-      return null;
-    }else{
-      control.setErrors({ nameError: "Le nom entré n'existe pas" });
-      return { nameError: "Le nom entré n'existe pas" };
+
+    validateAvance(control: AbstractControl): ValidationErrors | null {
+        const avance = control.value;
+        if (this.StuAvance && avance && avance < this.StuAvance) {
+            control.setErrors({ avanceError: "Attention ! L'élève va aller à un niveau précédent !" });
+            return { avanceError: "Attention ! L'élève va aller à un niveau précédent !" };
+        } else {
+            return null;
+        }
     }
-  }
 
-  validateAvance(control: AbstractControl):ValidationErrors | null{
-    const avance = control.value;
-    if (this.StuAvance && avance && avance<this.StuAvance){
-      control.setErrors({ avanceError: "Attention ! L'élève va aller à un niveau précédent !" });
-      return { avanceError: "Attention ! L'élève va aller à un niveau précédent !" };
-    }else{
-      return null
+    private _filter(value: string | null): string[] {
+        if (value) {
+            const filterValue = value.toLowerCase();
+            return this.studientsList.filter((stu) => stu.toLowerCase().includes(filterValue));
+        }
+        return this.studientsList;
     }
-  }
 
-  private _filter(value: string | null): string[] {
-    if (value){
-      const filterValue = value.toLowerCase();
-      return this.studientsList.filter(stu => stu.toLowerCase().includes(filterValue));
+    onSubmit() {
+        window.location.href = `/session/${this.sessionId}/lookup/`;
     }
-    return this.studientsList
-  }
-
-
-  onSubmit(){
-    window.location.href = `/session/${this.sessionId}/lookup/`;
-  }
 }
