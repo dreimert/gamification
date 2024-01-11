c7b39eee5b4942731e390bc0f9bfaa2a9d9695ca
Aaabdou <101267176+Aaabdou@users.noreply.github.com>
2024-01-11 15:05:45 +0100

    created route to tp kaka

---
+++ front/src/app/tpPage/tp-kafka/tp-kafka.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "app-tp-kafka",
+    standalone: true,
+    imports: [],
+    templateUrl: "./tp-kafka.component.html",
+    styleUrl: "./tp-kafka.component.css",
+})
+export class TpKafkaComponent {}
