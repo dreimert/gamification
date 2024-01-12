c8b9d1da194d5196620269802ba3aa9343820d0a
ghoti-GUI <guichijin@gmail.com>
2024-01-12 15:45:26 +0100

    update edit session ongoing

---
+++ front/src/app/mainpage/session/edit-session-programmed/edit-session-programmed.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-session-programmed',
+  standalone: true,
+  imports: [],
+  templateUrl: './edit-session-programmed.component.html',
+  styleUrl: './edit-session-programmed.component.css'
+})
+export class EditSessionProgrammedComponent {
+
+}
