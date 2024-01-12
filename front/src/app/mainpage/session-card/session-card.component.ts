27db00133bd1c0fc0d57be6d75e452ea71700334
ghoti-GUI <guichijin@gmail.com>
2024-01-12 15:45:26 +0100

    update edit session ongoing

--- front/src/app/mainpage/session-card/session-card.component.ts
+++ front/src/app/mainpage/session-card/session-card.component.ts
@@ -103,7 +103,15 @@ export class SessionCardComponent implements OnInit {
     gotoProgression(session: Session | TeacherSession) {
         this.router.navigateByUrl(`/session/${session.id}/progressions`, { state: session });
     }
+<<<<<<< HEAD
     gotoEditSession(session: Session | TeacherSession) {
         this.router.navigateByUrl(`/session/${session.id}/editSession`, { state: session });
+=======
+    gotoEditOngoing(session: Session | TeacherSession) {
+        this.router.navigateByUrl(`/session/${session.id}/editSessionOngoing`, { state: session });
+    }
+    gotoEditProgrammed(session: Session | TeacherSession) {
+        this.router.navigateByUrl(`/session/${session.id}/editSessionProgrammed`, { state: session });
+>>>>>>> aaaed78 (update edit session ongoing)
     }
 }
--- front/src/app/mainpage/session/edit-session-ongoing/edit-session-ongoing.component.spec.ts
+++ front/src/app/mainpage/session/edit-session-ongoing/edit-session-ongoing.component.spec.ts
@@ -1,23 +1,24 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { EditSessionOngoingComponent } from './edit-session-ongoing.component';
+import { EditSessionOngoingComponent } from "./edit-session-ongoing.component";
+import { RouterTestingModule } from "@angular/router/testing";
 
-describe('EditSessionOngoingComponent', () => {
-  let component: EditSessionOngoingComponent;
-  let fixture: ComponentFixture<EditSessionOngoingComponent>;
+describe("EditSessionOngoingComponent", () => {
+    let component: EditSessionOngoingComponent;
+    let fixture: ComponentFixture<EditSessionOngoingComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [EditSessionOngoingComponent]
-    })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(EditSessionOngoingComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(async () => {
+        window.history.pushState({ id: "1", name: "TP", password:'password', endDate:'2025-12-11T11:03:00.000Z' }, "", "");
+        await TestBed.configureTestingModule({
+            imports: [EditSessionOngoingComponent, RouterTestingModule],
+        }).compileComponents();
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+        fixture = TestBed.createComponent(EditSessionOngoingComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it("should create", () => {
+        expect(component).toBeTruthy();
+    });
 });
