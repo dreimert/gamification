311ac6d94b84d929ac11e48a0803768e7b958f7d
ghoti-GUI <guichijin@gmail.com>
2024-01-12 16:39:40 +0100

    update edit

---
+++ front/src/app/mainpage/session/edit-session/edit-session.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EditSessionComponent } from './edit-session.component';
+import { RouterTestingModule } from '@angular/router/testing';
+
+describe('EditSessionComponent', () => {
+  let component: EditSessionComponent;
+  let fixture: ComponentFixture<EditSessionComponent>;
+
+  beforeEach(async () => {
+    window.history.pushState({ id: "1", name: "TP", password:'password', endDate:'2025-12-11T11:03:00.000Z' }, "", "");
+    await TestBed.configureTestingModule({
+      imports: [EditSessionComponent, RouterTestingModule]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(EditSessionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
