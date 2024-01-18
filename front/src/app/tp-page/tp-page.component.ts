2a59dd9eb2037416d292009e6b0193c6844d3db5
Aaabdou <101267176+Aaabdou@users.noreply.github.com>
2024-01-18 21:06:28 +0100

    get session Info

--- front/src/app/tp-page/tp-page.component.ts
+++ front/src/app/tp-page/tp-page.component.ts
@@ -1,10 +1,10 @@
 import { Component } from "@angular/core";
 import { OnInit } from "@angular/core";
-import { ActivatedRoute, ParamMap } from "@angular/router";
-import { Observable } from "rxjs";
-import { switchMap } from "rxjs/operators";
+import { ActivatedRoute } from "@angular/router";
 import { TpKafkaComponent } from "./tp-kafka/tp-kafka.component";
 import { CommonModule } from "@angular/common";
+import { Session, TeacherSession } from "../models/session.model";
+import { SessionService } from "../services/session.service";
 @Component({
     selector: "app-tp-page",
     standalone: true,
@@ -13,17 +13,24 @@ import { CommonModule } from "@angular/common";
     styleUrl: "./tp-page.component.css",
 })
 export class TpPageComponent implements OnInit {
-    tpId!: Observable<string>;
+    tpId!: string;
     tp = tp;
-    constructor(private route: ActivatedRoute) {}
+    session!: Session;
+    constructor(
+        private route: ActivatedRoute,
+        private sessionService: SessionService,
+    ) {}
     ngOnInit() {
-        this.tpId = this.route.paramMap.pipe(switchMap((params: ParamMap) => params.get("id")!));
+        this.route.params.subscribe((params) => (this.tpId = params["id"]));
         this.fetchSessionInfo(this.tpId);
     }
-    fetchSessionInfo(id: Observable<string>) {
-        console.log(id);
+    fetchSessionInfo(id: string) {
+        this.sessionService.getAvailableSessions().subscribe((sessions: Session[] | TeacherSession[]) => {
+            this.session = sessions.find((element) => element.id === this.tpId) as Session;
+        });
+        this.session.TP = "kafka";
+        console.log("id: " + id);
+        // console.log(("sid: " + this.session.id) as string);
     }
 }
-const tp = {
-    type: "kafka",
-};
+const tp = "kafka";
