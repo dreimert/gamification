5b9690dd43e290210d0a903f52fd380a9291e0db
ghoti-GUI <guichijin@gmail.com>
2024-01-18 09:03:00 +0100

    reload image, midify: sidebar, session img

--- front/src/app/mainpage/grade-teacher/list-session-grade/edit-grade/edit-grade.component.ts
+++ front/src/app/mainpage/grade-teacher/list-session-grade/edit-grade/edit-grade.component.ts
@@ -137,4 +137,8 @@ export class EditGradeComponent implements OnInit {
     gotoListGrade() {
         this.router.navigateByUrl(`/grade-teacher/${this.session.id}/lookup`, { state: this.session });
     }
+    
+    return(){
+        history.back();
+    }
 }
--- front/src/app/mainpage/session/progression/progression.ts
+++ front/src/app/mainpage/session/progression/progression.ts
@@ -25,6 +25,86 @@ export const progressions: progression[] = [
         name: "student 4",
         level: "2",
     },
+    {
+        id: 5,
+        name: "student 5",
+        level: "2",
+    },
+    {
+        id: 6,
+        name: "student 6",
+        level: "2",
+    },
+    {
+        id: 7,
+        name: "student 7",
+        level: "2",
+    },
+    {
+        id: 8,
+        name: "student 8",
+        level: "2",
+    },
+    {
+        id: 9,
+        name: "student 9",
+        level: "2",
+    },
+    {
+        id: 10,
+        name: "student 10",
+        level: "2",
+    },
+    {
+        id: 11,
+        name: "student 11",
+        level: "2",
+    },
+    {
+        id: 12,
+        name: "student 12",
+        level: "2",
+    },
+    {
+        id: 13,
+        name: "student 13",
+        level: "2",
+    },
+    {
+        id: 14,
+        name: "student 14",
+        level: "2",
+    },
+    {
+        id: 15,
+        name: "student 15",
+        level: "2",
+    },
+    {
+        id: 16,
+        name: "student 16",
+        level: "2",
+    },
+    {
+        id: 17,
+        name: "student 17",
+        level: "2",
+    },
+    {
+        id: 18,
+        name: "student 18",
+        level: "2",
+    },
+    {
+        id: 19,
+        name: "student 19",
+        level: "2",
+    },
+    {
+        id: 20,
+        name: "student 20",
+        level: "2",
+    },
 ];
 
 export interface Levels {
