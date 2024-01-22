8ade7e85c6ab7b9c6c1458efb04e626fd833d73a
ghoti-GUI <guichijin@gmail.com>
2024-01-23 00:10:49 +0100

    show session name and date

--- front/src/app/tp-page/tp-scrapping/tp-scrapping.component.ts
+++ front/src/app/tp-page/tp-scrapping/tp-scrapping.component.ts
@@ -1,26 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
 import { Title } from "@angular/platform-browser";
 import { CommonModule } from "@angular/common";
 import { MatDialog } from "@angular/material/dialog";
+import { NgxTypedJsModule } from 'ngx-typed-js';
+import { timer } from 'd3';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { timer } from 'rxjs';
 import { DialogHelpComponent } from './dialog-help/dialog-help.component';
+import { Session } from '../../models/session.model';
 
 @Component({
   selector: 'app-tp-scrapping',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, FormsModule],
+  imports: [CommonModule, NgxTypedJsModule, FormsModule, ReactiveFormsModule],
   templateUrl: './tp-scrapping.component.html',
   styleUrl: './tp-scrapping.component.css'
 })
-export class TpScrappingComponent implements OnInit {
-    lvls: string[] = ['lvl1', 'lvl1', 'lvl2'];
-    lvl!:number
-    studentName: string='ghoti';
+export class TpScrappingComponent implements OnInit{
+    @Input() session!: Session | undefined;
+    sentences: string[] = [];
+    lvl!: number;
     passcodeList!:string[];
     passcode!:string;
     codeCorrect:boolean = false;
     codeEntered:boolean = false;
+    date!:string | undefined
 
     constructor(
         private titleService: Title,
@@ -28,36 +31,46 @@ export class TpScrappingComponent implements OnInit {
     ) {
         this.titleService.setTitle("Tp Scrapping");
     }
-
+    
     ngOnInit(): void {
         this.fetchLevel();
+        this.getDate();
+        this.loadSentences();
         this.fetchPasscode();
+
     }
 
+    getDate(){
+        this.date = new Date(this.session!.startDate).toISOString().split('T')[0]
+    }
 
     changeLevel() {
         this.lvl += 1;
+        console.log('lvl: ', this.lvl)
     }
     fetchLevel() {
-        // Service to get last level of student
         this.lvl = 0;
     }
+    // startLvl1() {
+    //     this.lvl = 1;
+    // }
     fetchPasscode(){
         // Service to get all passcode for all levels
-        this.passcodeList = ['lvl0', 'lvl1', 'lvl2', 'lvl3']
+        this.passcodeList = ['', 'lvl1', 'lvl2', 'lvl3']
     }
 
-    getEnteredPasscode(lvl:number){
+    getEnteredPasscode(currentLvl:number){
         this.codeEntered = true;
         // Service to valid passcode
-        if (this.passcode==this.passcodeList[lvl]){
+        if (this.passcode==this.passcodeList[currentLvl]){
             this.codeCorrect = true;
-            timer(1000).subscribe(() => {
+            setTimeout(() => {
                 this.changeLevel();
+                this.loadSentences();
                 this.codeEntered = false;
                 this.codeCorrect = false;
                 this.passcode = '';
-            });
+            }, 1000);
         }
     }
     stringToNumber(string:string): number{
@@ -69,5 +82,33 @@ export class TpScrappingComponent implements OnInit {
             width: "60%",
             height: "70%",
         });
+    }
+
+
+
+    loadSentences() {
+        switch (this.lvl) {
+            case 0:
+                this.sentences= [
+`${this.session?.name} [${this.date}]<br>Duree 2h<br>
+5TC:\\\Accueil> Bienvenue dans le tp scrapping! Vous ne me connaissez peut-être pas, mais j'ai besoin de votre aide en ce moment.
+5TC:\\\Accueil> Je suis en train de me connecter au serveur principal de l'INSA. J'ai besoin que vous m'aidiez à trouver les informations pertinentes pour m'aider à ... faire quelque chose. Je suis sûr que tu seras intéressé : )
+5TC:\\\Accueil> Pour vous aider, j'ai préparé un dossier contenant les informations dont vous avez besoin.
+        Appuyer sur <span class="text-yellow-200">'help'</span> pour plus d'informations.
+        Tapez <span class="text-yellow-200">\'Enter\'</span> pour continuer...`
+          ];
+                break;
+            case 1:
+                this.sentences= [
+`5TC:\\\Niveau_1 > C'est parti !
+5TC:\\\Niveau_1 > Je me connecte au système en tant que <span class="text-yellow-200">M. ABC</span>, mais je dois entrer son nom d'utilisateur. Pouvez-vous m'aider?
+        <span class="text-yellow-200">liste des enseignants:</span> <span class="text-blue-200">http://xxxxxxxxxxxx</span>
+        Entrer le nom de l'utilisateur : >`
+                              ];
+                break;
+            default:
+                this.sentences = ['默认句子数组', '默认句子数组的下一句', '...'];
+                break;
+        }
     }
 }
