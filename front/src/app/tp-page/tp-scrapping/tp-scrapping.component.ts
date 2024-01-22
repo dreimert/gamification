d6ac8f59f9bd577ae0c2ce5f33f83364b7b7bbc3
Xinyi ZHAO <105914398+xinyi2hao@users.noreply.github.com>
2024-01-22 01:15:47 +0100

    terminal window

--- front/src/app/tp-page/tp-scrapping/tp-scrapping.component.ts
+++ front/src/app/tp-page/tp-scrapping/tp-scrapping.component.ts
@@ -1,14 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from "@angular/platform-browser";
+import { CommonModule } from "@angular/common";
+import { MatDialog } from "@angular/material/dialog";
 
 @Component({
   selector: 'app-tp-scrapping',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './tp-scrapping.component.html',
   styleUrl: './tp-scrapping.component.css'
 })
-export class TpScrappingComponent implements OnInit{
+export class TpScrappingComponent implements OnInit {
+    lvl!: number;
+    constructor(
+        private titleService: Title,
+        public dialog: MatDialog,
+    ) {
+        this.titleService.setTitle("Tp Scrapping");
+    }
     ngOnInit(): void {
-
+        this.fetchLevel();
+    }
+    changeLevel() {
+        this.lvl += 1;
+    }
+    fetchLevel() {
+        // Service to get last level of student
+        this.lvl = 0;
     }
 }
