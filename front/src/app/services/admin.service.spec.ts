4f8032dbee93c6cd59b18832917d1add5ef32d61
28Pollux28 <lemaire_valentin28@yahoo.fr>
2024-01-22 19:54:41 +0100

    lint and tests
    Signed-off-by: 28Pollux28 <lemaire_valentin28@yahoo.fr>

--- front/src/app/services/admin.service.spec.ts
+++ front/src/app/services/admin.service.spec.ts
@@ -1,16 +1,20 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from "@angular/core/testing";
 
-import { AdminService } from './admin.service';
+import { AdminService } from "./admin.service";
+import { HttpClientModule } from "@angular/common/http";
 
-describe('AdminService', () => {
-  let service: AdminService;
+describe("AdminService", () => {
+    let service: AdminService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(AdminService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpClientModule],
+            providers: [AdminService],
+        });
+        service = TestBed.inject(AdminService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it("should be created", () => {
+        expect(service).toBeTruthy();
+    });
 });
