f4c0a1df1e05fd95c7550abf65732eec685c8ce1
ghoti-GUI <guichijin@gmail.com>
2024-01-22 11:34:03 +0100

    disappear text one by one

--- front/src/app/tp-page/tp-scrapping/tp-scrapping.component.ts
+++ front/src/app/tp-page/tp-scrapping/tp-scrapping.component.ts
@@ -12,15 +12,33 @@ import { MatDialog } from "@angular/material/dialog";
 })
 export class TpScrappingComponent implements OnInit {
     lvl!: number;
+    text:string='5TC:\> Bienvenue dans le tp scrapping! Vous ne me connaissez peut-être pas, mais j\'ai besoin de votre aide en ce moment';
+    textArray: string[] = [];
+
     constructor(
         private titleService: Title,
         public dialog: MatDialog,
     ) {
         this.titleService.setTitle("Tp Scrapping");
     }
+    
     ngOnInit(): void {
         this.fetchLevel();
+        
+        this.textArray=this.text.split('');
+
+        let timer = setInterval(()=>{
+            let text = document.querySelector('p')?.innerHTML;
+            text = this.textArray.pop()
+        }, 100)
+
     }
+
+    showtext(index:number): string {
+        return index < this.textArray.length ? 'inline' : 'none';
+    }
+
+
     changeLevel() {
         this.lvl += 1;
     }
