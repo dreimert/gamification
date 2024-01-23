b7a503b92e9fae5ca871b5a0b2e1e3c9911521ad
ghoti-GUI <guichijin@gmail.com>
2024-01-23 18:37:09 +0100

    change input style, method to show text

--- front/src/app/tp-page/tp-scrapping/tp-scrapping.component.ts
+++ front/src/app/tp-page/tp-scrapping/tp-scrapping.component.ts
@@ -7,6 +7,7 @@ import { timer } from 'd3';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DialogHelpComponent } from './dialog-help/dialog-help.component';
 import { Session } from '../../models/session.model';
+import { TpScrappingService } from '../../services/tp-scrapping.service';
 
 @Component({
   selector: 'app-tp-scrapping',
@@ -23,16 +24,20 @@ export class TpScrappingComponent implements OnInit{
     passcode!:string;
     codeCorrect:boolean = false;
     codeEntered:boolean = false;
-    date!:string | undefined
+    date!:string | undefined;
+    sentenceIndex:number = 0;
 
     constructor(
         private titleService: Title,
         public dialog: MatDialog,
+        // private tpService: TpScrappingService
     ) {
         this.titleService.setTitle("Tp Scrapping");
     }
     
     ngOnInit(): void {
+        // const initialsTeacher = this.tpService.getInitialsTeacher();
+        // console.log(initialsTeacher)
         this.fetchLevel();
         this.getDate();
         this.loadSentences(); 
@@ -50,9 +55,6 @@ export class TpScrappingComponent implements OnInit{
     fetchLevel() {
         this.lvl = 0;
     }
-    // startLvl1() {
-    //     this.lvl = 1;
-    // }
     fetchPasscode(){
         // Service to get all passcode for all levels
         this.passcodeList = [ 'start', 'lvl1', 'lvl2', 'lvl3']
@@ -64,6 +66,7 @@ export class TpScrappingComponent implements OnInit{
         if (this.passcode==this.passcodeList[currentLvl]){
             this.codeCorrect = true;
             setTimeout(() => {
+                this.sentenceIndex = 0;
                 this.changeLevel();
                 this.loadSentences();
                 this.codeEntered = false;
@@ -84,23 +87,26 @@ export class TpScrappingComponent implements OnInit{
     }
 
 
+    nextSentence(){
+        this.sentenceIndex+=1;
+    }
 
     loadSentences() {
         switch (this.lvl) {
             case 0:
                 this.sentences= [
-`${this.session?.name} [${this.date}]<br>Duree 2h<br>
-5TC:\\\Accueil> Bienvenue dans le tp scrapping! Vous ne me connaissez peut-être pas, mais j'ai besoin de votre aide en ce moment.
-5TC:\\\Accueil> Je suis en train de me connecter au serveur principal de l'INSA. J'ai besoin que vous m'aidiez à trouver les informations pertinentes pour m'aider à ... faire quelque chose. Je suis sûr que tu seras intéressé : )
-5TC:\\\Accueil> Pour vous aider, j'ai préparé un dossier contenant les informations dont vous avez besoin.
+
+`^1000Bienvenue dans le tp scrapping! Vous ne me connaissez peut-être pas, mais j'ai besoin de votre aide en ce moment.^100`, 
+`^1000Je suis en train de me connecter au serveur principal de l'INSA. J'ai besoin que vous m'aidiez à trouver les informations pertinentes pour m'aider à faire quelque chose. Je suis sûr que tu seras intéressé : )^100`, 
+`^1000Pour vous aider, j'ai préparé un dossier contenant les informations dont vous avez besoin.^500
         Appuyer sur <span class="text-yellow-200">'help'</span> pour plus d'informations.
-        Tapez <span class="text-yellow-200">\'start\'</span> pour continuer...`
+        Tapez <span class="text-yellow-200">\'start\'</span> pour continuer : `
           ];
                 break;
             case 1:
                 this.sentences= [
-`5TC:\\\Niveau_1 > C'est parti !
-5TC:\\\Niveau_1 > Je me connecte au système en tant que <span class="text-yellow-200">M. ABC</span>, mais je dois entrer son nom d'utilisateur. Pouvez-vous m'aider?
+`^1000C'est parti !^100`, 
+`^1000Je me connecte au système en tant que <span class="text-yellow-200">M. ABC</span>, mais je dois entrer son nom d'utilisateur. Pouvez-vous m'aider?^100
         <span class="text-yellow-200">liste des enseignants:</span> <span class="text-blue-200">http://xxxxxxxxxxxx</span>
         Entrer le nom de l'utilisateur : >`
                               ];
