722b299fd153592dc16e858df392c46b80c1eedb
Aaabdou <101267176+Aaabdou@users.noreply.github.com>
2024-01-27 09:32:11 +0100

    added endTime for bonus vote

--- front/src/app/mainpage/grade/grade.component.ts
+++ front/src/app/mainpage/grade/grade.component.ts
@@ -8,6 +8,8 @@ import { Router } from "@angular/router";
 import { MatDialog } from "@angular/material/dialog";
 import { HelpingBonusComponent } from "./helping-bonus/helping-bonus.component";
 import { MatTooltipModule } from "@angular/material/tooltip";
+import { User } from "../../models/user.model";
+import { SessionService } from "../../services/session.service";
 
 @Component({
     selector: "app-grade",
@@ -27,6 +29,7 @@ export class GradeComponent implements OnInit {
         private gradeService: GradeService,
         private router: Router,
         private dialog: MatDialog,
+        private sessionService: SessionService,
     ) {}
 
     ngOnInit() {
@@ -41,10 +44,51 @@ export class GradeComponent implements OnInit {
         });
     }
     popupBonus(grade: StudentGrade) {
-        console.log(grade);
-        this.dialog.open(HelpingBonusComponent, {
-            width: "60%",
-            data: grade,
+        let editTimeEnded: boolean;
+        const date = new Date();
+        date.setHours(date.getHours() - date.getTimezoneOffset() / 60);
+        this.sessionService.getSession(grade.sessionId).subscribe({
+            next: (session) => {
+                const endDate = new Date(session.endDate);
+                editTimeEnded = endDate.getTime() < date.getTime();
+                if (editTimeEnded) {
+                    this.gradeService.getBonus(grade.progressionId).subscribe({
+                        next: (students: User[]) => {
+                            if (students.length >= 2) {
+                                alert(
+                                    "Vous ne pouvez plus voter.\nVous avez voté pour " +
+                                        this.studentName(students[0]) +
+                                        " et " +
+                                        this.studentName(students[1]) +
+                                        ".",
+                                );
+                            } else if (students.length == 1) {
+                                alert(
+                                    "Vous ne pouvez plus voter.\nVous avez voté pour " +
+                                        this.studentName(students[0]) +
+                                        ".",
+                                );
+                            } else {
+                                alert("Vous ne pouvez plus voter.\nVous n'avez voté pour personne.");
+                            }
+                        },
+                        error: (err) => {
+                            console.log(err);
+                        },
+                    });
+                } else {
+                    this.dialog.open(HelpingBonusComponent, {
+                        width: "60%",
+                        data: grade,
+                    });
+                }
+            },
+            error: (err) => {
+                console.log(err);
+            },
         });
+    }
+    studentName(student: User | undefined) {
+        return student === undefined ? null : student.name + " " + student.surname;
     }
 }
